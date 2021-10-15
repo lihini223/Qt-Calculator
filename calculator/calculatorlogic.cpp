b3d16ca2eaa2294e0aa1bc75d62fc5b1b3aaf81d
Janith Harinda <jharinda@gmail.com>
2021-10-16 00:49:06 +0530

    handled divided by zero exception

--- calculator/calculatorlogic.cpp
+++ calculator/calculatorlogic.cpp
@@ -1,4 +1,5 @@
 #include "calculatorlogic.h"
+#include <iostream>
 
 CalculatorLogic::CalculatorLogic()
 {
@@ -14,7 +15,16 @@ int CalculatorLogic::subtract (int num1, int num2) {
 }
 
 float CalculatorLogic::division (int num1, int num2) {
-    return float(num1) / float(num2);
+
+    float result;
+    try {
+         result  = num1/num2;
+
+    }  catch (std::exception e) {
+        result = 0;
+    }
+
+    return result;
 }
 
 int CalculatorLogic::multiplication (int num1, int num2) {
