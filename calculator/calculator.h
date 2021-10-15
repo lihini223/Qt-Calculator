d764c267dbc448218f7c2512e3930206a16797ab
unknown <khsnsugeeshwara@students.nsbm.ac.lk>
2021-10-15 18:47:38 +0530

    add calculator addition and subtraction functions

--- calculator/calculator.h
+++ calculator/calculator.h
@@ -15,6 +15,35 @@ public:
     calculator(QWidget *parent = nullptr);
     ~calculator();
 
+private slots:
+    void on_btnNum1_clicked();
+
+    void on_btnNum2_clicked();
+
+    void on_btnNum3_clicked();
+
+    void on_btnNum4_clicked();
+
+    void on_btnNum5_clicked();
+
+    void on_btnNum6_clicked();
+
+    void on_btnNum7_clicked();
+
+    void on_btnNum8_clicked();
+
+    void on_btnNum9_clicked();
+
+    void on_btnNum0_clicked();
+
+    void on_btnEqual_clicked();
+
+    void on_btnAdd_clicked();
+
+    void on_btnSubtract_clicked();
+
+    void on_btnClear_clicked();
+
 private:
     Ui::calculator *ui;
 };
